289135f5827c9fb4da8deea145ea201c2f2cd7d5
WeslleyIvis <weslley_reive@hotmail.com>
2023-06-21 04:57:03 -0300

    class pokeAPI, get 151 pokemons, gerate pokes

--- modules/Pokedex.js
+++ modules/Pokedex.js
@@ -1,67 +1,80 @@
+import CreateComponents from './CreateComponents.js';
+
 export default class Pokedex {
-  constructor(pokemon, section) {
-    this.pokemon = pokemon.toLowerCase();
-    this.container = section;
-    this.data = {};
+  constructor(section) {
+    this.generator = new CreateComponents();
+    this.container = document.querySelector(section);
+    this.dataPokedex = [];
   }
 
-  async getPokemonAPI(pokemon) {
-    await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-      .then((r) => r.json())
-      .then((data) => {
-        this.data.pokemon = data;
-        this.getPokemonStatus(this.data.pokemon.species.url);
-      })
-      .catch((error) => console.error({ error: error }));
+  async fetchPokemons(limitPokedex) {
+    const data = await fetch(
+      `https://pokeapi.co/api/v2/pokemon?limit=${limitPokedex}`,
+    ).then((r) => r.json());
+    const dataPokemon = data.results;
+
+    for (const pokemon of dataPokemon) {
+      const pokemonDetails = await this.fetchPokemonsStatus(pokemon.url);
+      this.dataPokedex.push(pokemonDetails);
+    }
+
+    console.log(this.dataPokedex);
+    this.container.appendChild(this.createPokedex(this.dataPokedex));
   }
 
-  async getPokemonStatus(url) {
-    await fetch(url)
-      .then((r) => r.json())
-      .then((status) => {
-        this.data.status = status;
-        this.__createInterfacePokemon();
-      });
+  async fetchPokemonsStatus(url) {
+    const r = await fetch(url);
+    const data = await r.json();
+    return data;
   }
 
-  __createInterfacePokemon() {
-    console.log(this.data);
+  createPokedex(data) {
+    const content = this.generator.createNode(
+      'section',
+      null,
+      `pokedex-content`,
+    );
 
-    this.container.appendChild(this.createNode('h1', this.data.pokemon.name));
-    this.container.appendChild(
-      this.createNode(
-        'img',
+    data.forEach((element, i) => {
+      const cardPokemon = this.generator.createNode(
+        'div',
         null,
-        'poke-img',
-        this.data.pokemon.sprites.other.dream_world.front_default,
-      ),
-    );
-    this.container.appendChild(
-      this.createNode('p', `Pokedex N°: ${this.data.pokemon.id}`),
-    );
-    this.container.appendChild(
-      this.createNode('p', this.data.status.generation.name),
-    );
-
-    this.data.pokemon.types.forEach((element) => {
-      this.container.appendChild(
-        this.createNode('span', `| ${element.type.name} `),
+        `pokedex-card`,
       );
+      //cardPokemon.classList.add(`${element.types[0].type.name}`);
+      cardPokemon.appendChild(
+        this.generator.createNode(
+          'h1',
+          // deixa a primeira letra em Maiusculo
+          element.name.charAt(0).toUpperCase() + element.name.slice(1),
+          'poke-name',
+        ),
+      );
+      cardPokemon.appendChild(
+        this.generator.createNode('span', element.id, 'poke-id'),
+      );
+      const boxIMG = this.generator.createNode('div', null, 'poke-box-img');
+      boxIMG.appendChild(
+        this.generator.createNode('img', null, 'poke-img', {
+          attribute: 'src',
+          value: element.sprites.other['official-artwork'].front_default,
+        }),
+      );
+      const boxTYPES = this.generator.createNode('div', null, 'poke-types');
+      element.types.forEach((element) => {
+        boxTYPES.appendChild(
+          this.generator.createNode(
+            'span',
+            element.type.name,
+            element.type.name,
+          ),
+        );
+      });
+      cardPokemon.appendChild(boxIMG);
+      cardPokemon.appendChild(boxTYPES);
+      content.appendChild(cardPokemon);
     });
-
-    console.log(this.data.status.generation.name);
-  }
-
-  createNode(nodeType, value = null, className = null, src = null) {
-    const node = document.createElement(nodeType);
-    value ? (node.innerText = value) : null;
-    className ? node.classList.add(className) : null;
-    src ? node.setAttribute('src', src) : null;
-
-    return node;
-  }
-
-  initPokemon() {
-    this.getPokemonAPI(this.pokemon);
+    console.log(content);
+    return content;
   }
 }
