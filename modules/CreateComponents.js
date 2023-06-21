098758742b90a29b3ed94b61612cf650674e0ed6
WeslleyIvis <weslley_reive@hotmail.com>
2023-06-21 04:56:30 -0300

    class create node element

--- modules/CreateComponents.js
+++ modules/CreateComponents.js
@@ -1,1 +1,17 @@
-export default class CreateComponents {}
+export default class CreateComponents {
+  createNode(
+    nodeType,
+    value = null,
+    className = null,
+    src = { attribute: null, value: null },
+  ) {
+    const node = document.createElement(nodeType);
+    value ? (node.innerText = value) : null;
+    className ? node.classList.add(className) : null;
+    src.attribute && src.value
+      ? node.setAttribute(src.attribute, src.value)
+      : null;
+
+    return node;
+  }
+}
