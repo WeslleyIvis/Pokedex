389343986826e98ff91634e5fed1fb4a5d90d835
WeslleyIvis <weslley_reive@hotmail.com>
2023-06-25 04:17:29 -0300

    feature: fetch pokedex

--- script/modules/Pokedex.js
+++ script/modules/Pokedex.js
@@ -1,11 +1,12 @@
 import CreateComponents from './CreateComponents.js';
 import Modal from './Modal.js';
 export default class Pokedex {
-  constructor(section) {
-    this.generator = new CreateComponents();
+  constructor(main) {
+    this.creatorNode = new CreateComponents();
     this.modal = new Modal();
-    this.container = document.querySelector(section);
+    this.container = document.querySelector(main);
     this.dataPokedex = [];
+    this.contentPokedex = null;
     this.typeColors = [
       {
         type: 'fire',
@@ -80,6 +81,75 @@ export default class Pokedex {
         color: 'rgb(90 84 101)',
       },
     ];
+    this.generations = [
+      {
+        geration: 'kanto',
+        quantityPokedex: {
+          start: 1,
+          end: 151,
+        },
+      },
+      {
+        geration: 'jhoto',
+        quantityPokedex: {
+          start: 152,
+          end: 99,
+        },
+      },
+      {
+        geration: 'hoenn',
+        quantityPokedex: {
+          start: 252,
+          end: 386,
+        },
+      },
+      {
+        geration: 'sinnoh',
+        quantityPokedex: {
+          start: 387,
+          end: 493,
+        },
+      },
+      {
+        geration: 'unova',
+        quantityPokedex: {
+          start: 494,
+          end: 649,
+        },
+      },
+      {
+        geration: 'kalos',
+        quantityPokedex: {
+          start: 650,
+          end: 721,
+        },
+      },
+      {
+        geration: 'alola',
+        quantityPokedex: {
+          start: 722,
+          end: 809,
+        },
+      },
+      {
+        geration: 'galar',
+        quantityPokedex: {
+          start: 810,
+          end: 898,
+        },
+      },
+    ];
+  }
+
+  async fetchPokeGeration(start, quantity) {
+    const pokemon = await fetch(
+      `https://pokeapi.co/api/v2/pokemon?offset=${start}&limit=${quantity}`,
+    )
+      .then((data) => data.json())
+      .catch((err) => console.error(err));
+
+    console.log(pokemon);
+    return pokemon;
   }
 
   async fetchPokemons(limitPokedex) {
@@ -87,7 +157,7 @@ export default class Pokedex {
       `https://pokeapi.co/api/v2/pokemon?limit=${limitPokedex}`,
     ).then((r) => r.json());
     const dataPokemon = data.results;
-    const loading = this.generator.createNode('div', 'loader');
+    const loading = this.creatorNode.createNode('div', 'loader');
     this.container.appendChild(loading);
 
     for (const pokemon of dataPokemon) {
@@ -97,8 +167,6 @@ export default class Pokedex {
 
     this.container.appendChild(this.createPokedex(this.dataPokedex));
     this.container.removeChild(document.querySelector('.loader'));
-
-    console.log(this.dataPokedex);
 
     return this.dataPokedex;
   }
@@ -121,14 +189,14 @@ export default class Pokedex {
   }
 
   createPokedex(data) {
-    const content = this.generator.createNode('section', `pokedex-content`);
+    const content = this.creatorNode.createNode('section', `pokedex-content`);
 
     data.forEach((element, i) => {
-      const cardPokemon = this.generator.createNode('div', `pokedex-card`);
+      const cardPokemon = this.creatorNode.createNode('div', `pokedex-card`);
       // \/ Pinta o background dos cards com a cor do elemento do pokemon
       //cardPokemon.classList.add(`${element.types[0].type.name}`);
       cardPokemon.appendChild(
-        this.generator.createNode(
+        this.creatorNode.createNode(
           'h1',
           // deixa a primeira letra em Maiusculo
           'poke-name',
@@ -136,19 +204,19 @@ export default class Pokedex {
         ),
       );
       cardPokemon.appendChild(
-        this.generator.createNode('span', 'poke-id', element.id),
+        this.creatorNode.createNode('span', 'poke-id', element.id),
       );
-      const boxIMG = this.generator.createNode('div', 'poke-box-img');
+      const boxIMG = this.creatorNode.createNode('div', 'poke-box-img');
       boxIMG.appendChild(
-        this.generator.createNode('img', 'poke-img', null, {
+        this.creatorNode.createNode('img', 'poke-img', null, {
           attribute: 'src',
           value: element.sprites.other['official-artwork'].front_default,
         }),
       );
-      const boxTYPES = this.generator.createNode('div', 'poke-types');
+      const boxTYPES = this.creatorNode.createNode('div', 'poke-types');
       element.types.forEach((element) => {
         boxTYPES.appendChild(
-          this.generator.createNode(
+          this.creatorNode.createNode(
             'span',
             element.type.name,
             element.type.name,
@@ -157,30 +225,36 @@ export default class Pokedex {
       });
       cardPokemon.appendChild(boxIMG);
       cardPokemon.appendChild(boxTYPES);
+
+      this.selectPokemon(cardPokemon, cardPokemon.childNodes[1].innerText);
+
       content.appendChild(cardPokemon);
     });
-    return content;
+    return (this.contentPokedex = content);
   }
 
   createPokemon(element) {
-    const modal = this.generator.createNode('section', 'modal');
-    const componentModal = this.generator.createNode('div', 'comp-modal');
-    const contentImage = this.generator.createNode('div', 'modal-poke-image');
-    const contentStatus = this.generator.createNode('div', 'modal-poke-status');
+    const modal = this.creatorNode.createNode('section', 'modal');
+    const componentModal = this.creatorNode.createNode('div', 'comp-modal');
+    const contentImage = this.creatorNode.createNode('div', 'modal-poke-image');
+    const contentStatus = this.creatorNode.createNode(
+      'div',
+      'modal-poke-status',
+    );
 
     // Get element color
     const pokeColor = this.getColorElementColor(element.types[0].type.name);
     contentImage.style.backgroundImage = `linear-gradient(to top, transparent 0%, ${pokeColor} 100%)`;
 
     // Arrow button
-    const arrow = this.generator.createButton('←', 'modal-arrow');
+    const arrow = this.creatorNode.createButton('←', 'modal-arrow');
     this.modal.closeModal(arrow);
 
     contentImage.appendChild(arrow);
 
     // Id pokedex
     contentImage.appendChild(
-      this.generator.createNode(
+      this.creatorNode.createNode(
         'div',
         'modal-id-pokedex',
         `# ${element.id.toString().padStart(4, '0')}`,
@@ -188,9 +262,9 @@ export default class Pokedex {
     );
 
     // Content image
-    const image = this.generator.createNode('div', 'modal-cont-img');
+    const image = this.creatorNode.createNode('div', 'modal-cont-img');
     image.appendChild(
-      this.generator.createNode('img', 'modal-poke-img', null, {
+      this.creatorNode.createNode('img', 'modal-poke-img', null, {
         attribute: 'src',
         value: element.sprites.other['official-artwork'].front_default,
       }),
@@ -199,9 +273,9 @@ export default class Pokedex {
     contentImage.appendChild(image);
 
     // Button get shiny pokemon
-    const buttonShiny = this.generator.createButton(null, 'button-shiny');
+    const buttonShiny = this.creatorNode.createButton(null, 'button-shiny');
     buttonShiny.appendChild(
-      this.generator.createNode('img', '', undefined, {
+      this.creatorNode.createNode('img', '', undefined, {
         attribute: 'src',
         value: 'https://static.thenounproject.com/png/3386874-200.png',
       }),
@@ -212,7 +286,7 @@ export default class Pokedex {
 
     // H1 name pokemon
     contentStatus.appendChild(
-      this.generator.createNode(
+      this.creatorNode.createNode(
         'h1',
         'modal-poke-name',
         element.name.charAt(0).toUpperCase() + element.name.slice(1),
@@ -220,10 +294,10 @@ export default class Pokedex {
     );
 
     // Content Type elements pokemon
-    const types = this.generator.createNode('div', 'modal-types');
+    const types = this.creatorNode.createNode('div', 'modal-types');
     element.types.forEach((element) => {
       types.appendChild(
-        this.generator.createNode(
+        this.creatorNode.createNode(
           'span',
           element.type.name,
           element.type.name.charAt(0).toUpperCase() +
@@ -235,30 +309,34 @@ export default class Pokedex {
     contentStatus.appendChild(types);
 
     // Weight & Height
-    const pokeSize = this.generator.createNode('div', 'size-pokemon');
+    const pokeSize = this.creatorNode.createNode('div', 'size-pokemon');
     pokeSize.appendChild(
-      this.generator.createNode('span', 'weight', `${element.weight / 10} KG`),
+      this.creatorNode.createNode(
+        'span',
+        'weight',
+        `${element.weight / 10} KG`,
+      ),
     );
     pokeSize.appendChild(
-      this.generator.createNode('span', 'height', `${element.height / 10} M`),
+      this.creatorNode.createNode('span', 'height', `${element.height / 10} M`),
     );
 
     contentStatus.appendChild(pokeSize);
 
     // Status bar
-    const baseStatus = this.generator.createNode('div', 'modal-base-status');
+    const baseStatus = this.creatorNode.createNode('div', 'modal-base-status');
     baseStatus.appendChild(
-      this.generator.createNode('h2', null, 'Base Status'),
+      this.creatorNode.createNode('h2', null, 'Base Status'),
     );
 
-    const boxStatus = this.generator.createNode('div', 'box-status');
+    const boxStatus = this.creatorNode.createNode('div', 'box-status');
 
     element.stats.forEach((status) => {
       boxStatus.appendChild(
-        this.generator.createNode('span', null, status.stat.name),
+        this.creatorNode.createNode('span', null, status.stat.name),
       );
-      const statusBar = this.generator.createNode('div', 'status-bar');
-      const statusAmount = this.generator.createNode(
+      const statusBar = this.creatorNode.createNode('div', 'status-bar');
+      const statusAmount = this.creatorNode.createNode(
         'div',
         status.stat.name,
         `${status.base_stat} / 300`,
@@ -279,6 +357,21 @@ export default class Pokedex {
     modal.appendChild(componentModal);
 
     return modal;
+  }
+
+  selectPokemon(node, id) {
+    node.addEventListener('click', () =>
+      this.createSelectedPokemon(id, this.contentPokedex),
+    );
+  }
+
+  async createSelectedPokemon(id, nodeDisable = null) {
+    const pokemon = await this.fetchPokemonName(id);
+    const organism = this.createPokemon(pokemon);
+
+    this.container.appendChild(organism);
+
+    nodeDisable ? this.creatorNode.toggleClass(nodeDisable) : null;
   }
 
   getColorElementColor(element) {
